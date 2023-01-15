b21b6b71c5b7ec6bae4cbf289089981db346f382
Bea <117057809+Milkmelona@users.noreply.github.com>
2023-01-15 15:16:22 +0800

    add grids and size slider

--- main.js
+++ main.js
@@ -1,9 +1,29 @@
-let board = document.querySelector('.board');
-board.style.gridTemplateColumns = 'repeat(16, 1fr)';
-board.style.gridTemplateRows = 'repeat(16, 1fr)';
+function adjustSize(size){
+let board = document.querySelector(".board");
+board.style.gridTemplateColumns = `repeat(${size}, 1fr)`
+board.style.gridTemplateRows = `repeat(${size}, 1fr)`
 
-for (i=0; i< 256; i++) {
-    let square = document.createElement('div');
-    square.style.backgroundColor = 'black';
-    board.insertAdjacentElement = ('beforeend', square);
+for (let i=0; i< (size * size); i++){
+    let square = document.createElement("div");
+    square.style.backgroundColor ="white";
+    board.insertAdjacentElement("beforeend", square);
+    square.addEventListener('mouseover', changeColor);
 }
+}
+function changeColor(e){
+    let colorPicked = document.getElementById('colorpicker');
+    e.target.style.backgroundColor = colorPicked.value
+}
+function clearColor() {
+    let board = document.querySelector(".board");
+    let squares = board.querySelectorAll("div");
+    squares.forEach((div) => (div.style.backgroundColor = "white"));
+}
+const slider = document.getElementById('slider');
+const sliderValue = document.getElementById('slidervalue');
+slider.onchange = e => updateSliderValue(e.target.value)
+function updateSliderValue (value) {
+    sliderValue.innerHTML = `${value} x ${value}`;
+    adjustSize(value);
+}
+
